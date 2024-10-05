3b0759b934429485266f0e48e293646ccf49d9a6
evo264714 <mh264714@gmail.com>
2024-10-05 10:42:31 +0600

    added cart system with modified origins

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const allowedOrigins = [
   "https://personal-collection-manager.web.app",
-  "http://localhost:5174",
+  "http://localhost:5173"
 ];
 
 app.use(
